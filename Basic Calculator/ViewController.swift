4f6598f9bec19e3ce00d8c870a0efad14fc011cf
Serdar Vatansever <serdarvatansever@serdar-macbook-pro.local>
2018-05-28 17:11:17 +0300

    Initial Commit

---
+++ Basic Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Basic Calculator
+//
+//  Created by Serdar Vatansever on 28.05.2018.
+//  Copyright © 2018 Serdar Vatansever. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
